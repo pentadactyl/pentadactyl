3905c10ad8e0da2b73760c97644df47604d7366e
Jack Rosenthal <jack@rosenth.al>
2019-07-08 19:14:14 -0600

    Delete melodactyl and teledactyl
    Nobody is around to support this code anymore. Delete it so we can
    focus our efforts on just Pentadactyl.
    
    BUG=#2

--- melodactyl/defaults/preferences/dactyl.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.dactyl.name", "melodactyl");
-pref("extensions.dactyl.appName", "Melodactyl");
-pref("extensions.dactyl.idName", "MELODACTYL");
-pref("extensions.dactyl.fileExt", "melo");
-pref("extensions.dactyl.host", "Songbird");
-pref("extensions.dactyl.hostbin", "songbird");
--- teledactyl/defaults/preferences/dactyl.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.dactyl.name", "teledactyl");
-pref("extensions.dactyl.appName", "Teledactyl");
-pref("extensions.dactyl.idName", "TELEDACTYL");
-pref("extensions.dactyl.fileExt", "tele");
-pref("extensions.dactyl.host", "Thunderbird");
-pref("extensions.dactyl.hostbin", "thunderbird");
